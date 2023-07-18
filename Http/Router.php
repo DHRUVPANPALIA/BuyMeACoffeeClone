9b9ceb96534696c599277487b2f2a137598f1ec6
Dhruv <21dit043@charusat.edu.in>
2023-07-18 16:13:37 +0530

    Project

--- Http/Router.php
+++ Http/Router.php
@@ -1,7 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BuymeAcoffeeClone\Kernel\Http;
 
 class Router{
-    
+    public static function execute(string $uri) {
+
+        $uri = '/' . trim($uri, charaters: '/');
+        $url = !empty($_GET['uri']) ? '/' . $_GET['uri'] : '/';
+
+    }
 }
