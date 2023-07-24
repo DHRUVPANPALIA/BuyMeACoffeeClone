fc168995b4453f6299a63c419228aec887dfd617
Your Name <you@example.com>
2023-07-24 15:33:22 +0530

    Changes

--- Http/Router.php
+++ Http/Router.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 namespace BuymeAcoffeeClone\Kernel\Http;
 
 class Router{
+    private const URI_REGGEX = '#^uri$#';
+    private const SEPERATOR  = '@'
+
     public static function execute(string $uri) {
 
         $uri = '/' . trim($uri, charaters: '/');
-        $url = !empty($_GET['uri']) ? '/' . $_GET['uri'] : '/';
+        $uri = '/';
+        $url .= !empty($_GET['uri']) ? $_GET['uri'] : '';
 
+        if (preg_match(pattern '#^uri$#', $url, &matches: $params)){
+
+        }
+
+    }
+
+    private function isController() {
+        return strpos($method, needle: self::SEPARATOR) !== false
     }
 }
--- src/Kernel/routes.php
+++ src/Kernel/routes.php
@@ -1,2 +1,3 @@
 <?php
 
+Router::get('MyController@action')
