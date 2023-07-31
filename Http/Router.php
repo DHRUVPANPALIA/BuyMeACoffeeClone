61daf6c4fd4e3919758cba33f3ed4fda45b14857
Your Name <you@example.com>
2023-07-31 14:36:51 +0530

    Router File Restored

---
+++ Http/Router.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuymeAcoffeeClone\Kernel\Http;
+
+use http\Exception\InvalidArgumentException;
+use ReflectionClass;
+use ReflectionException;
+
+class Router{
+
+    private const CONTROLLER_NAMESPACE = 'BuyMeACoffeeClone\Controller\\'
+    private const URI_REGGEX = '#^uri$#';
+    private const SEPARATOR  = '@';
+
+    public const METHOD_GET = 'GET';
+
+    public const METHOD_POST = 'POST';
+    private ?string $httpMethod = null;
+
+    public static function get(string $uri, string $classMethod = ''){
+        self::$httpMethod =  ::METHOD_GET;
+        self::execute($uri, $classMethod );
+    }
+
+    public static function post(string $uri, string $classMethod = '' ): void{
+        self::$httpMethod = self::METHOD_POST;
+        self::execute($uri, $classMethod );
+
+    }
+    public static function execute(string $uri, string $method) {
+
+        $uri = '/' . trim($uri, characters: '/');
+        $url = '/';
+        $url .= !empty($_GET['uri']) ? $_GET['uri'] : '';
+
+        if (preg_match( '#^uri$#', $url, $params)){
+            if (self::isController($method)) {
+                header('301');
+                header(
+                    sprintf('Location: %s/%s',  $_ENV['SITE_URL'], $method)
+                );
+            } else {
+                if (!self::isHttpMethodValid()){
+                    throw  new InvalidArgumentException(
+                        sprintf('Invalid  "%s" HTTP Request', $_SERVER['REQUEST_METHOD'])
+                    );
+                    exit('Invalid HTTP request');
+                } else {
+                    $split =  explode(self::SEPARATOR, $method);
+                    $className = self::CONTROLLER_NAMESPACE . $split [0];
+                    $method = $split[1];
+
+                    try {
+                        $reflection = new ReflectionClass();
+
+                        // Check if the class has method
+                        if (class_exists($className) && $className->hasMethod()){
+                            $action = new \ReflectionMethod($className, $method);
+
+                            // Make sure the requested action has "public" visibility
+                            if ($action->isPublic()) {
+                                // Now, we perform the controller's action
+                                return $action->invokeArgs(new $className, self::getActionParameters($params));
+                            }
+                        }
+                    } catch (ReflectionException $err) {
+                        // TODO Show the "Not Found" page
+                        echo $err->getMessage();
+
+                    }
+                    // TODO Show the "NOT  FOUND" page here.
+                }
+
+            }
+        }
+
+    }
+
+    private static function isHttpMethodValid(): bool{
+        return  self::$httpMethod !== null && $_SERVER['REQUEST_METHOD'] !== self::$httpMethod;
+    }
+
+    private static function getActionParameters(array $param) {
+        foreach ($params as $key => $value) {
+            $params[$key] = str_replace($value, '/', '');
+        }
+    }
+    private function isController(string $method): bool {
+        return strpos($method, self::SEPARATOR) !== false
+    }
+}
